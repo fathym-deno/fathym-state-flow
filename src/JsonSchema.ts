f42e12de8a05d8301691114e88ea16bc553394cd
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-28 06:06:47 +0000

    "Enhanced JsonSchema type in src/JsonSchema.ts for comprehensive representation"

--- src/JsonSchema.ts
+++ src/JsonSchema.ts
@@ -1,11 +1,43 @@
 export type JsonSchema = {
-  [key: string]: {
-    type: string;
-    properties?: {
-      [key: string]: {
-        type: string;
-      };
-    };
-    required?: string[];
+  $id?: string;
+  $schema?: string;
+  title?: string;
+  description?: string;
+  type?: string;
+  properties?: {
+    [key: string]: JsonSchema;
   };
+  additionalProperties?: boolean | JsonSchema;
+  required?: string[];
+  items?: JsonSchema | JsonSchema[];
+  minItems?: number;
+  maxItems?: number;
+  uniqueItems?: boolean;
+  enum?: any[];
+  default?: any;
+  format?: string;
+  $ref?: string;
+  definitions?: {
+    [key: string]: JsonSchema;
+  };
+  patternProperties?: {
+    [key: string]: JsonSchema;
+  };
+  dependencies?: {
+    [key: string]: JsonSchema | string[];
+  };
+  minimum?: number;
+  maximum?: number;
+  exclusiveMinimum?: number;
+  exclusiveMaximum?: number;
+  multipleOf?: number;
+  minLength?: number;
+  maxLength?: number;
+  pattern?: string;
+  minProperties?: number;
+  maxProperties?: number;
+  allOf?: JsonSchema[];
+  anyOf?: JsonSchema[];
+  oneOf?: JsonSchema[];
+  not?: JsonSchema;
 };
