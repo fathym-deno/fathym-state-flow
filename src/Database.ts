5f953385e023316c110849e2d18b9e6f8a333bb9
mcgear <michael.gearhardt@gmail.com>
2023-08-28 09:18:31 -0600

    Database cleaned out

--- src/Database.ts
+++ src/Database.ts
@@ -1,25 +1,7 @@
 import { IDataProvider } from "./IDataProvider.ts";
-import { JsonSchema } from "./JsonSchema.ts";
 
 export class Database<T> {
   constructor(
-    private dataProvider: IDataProvider<T>,
-    private schema: JsonSchema,
+    protected dataProvider: IDataProvider<T>,
   ) {}
-
-  async set(key: string, value: T): Promise<void> {
-    await this.dataProvider.set(key, value);
-  }
-
-  async get(key: string): Promise<T | null> {
-    return await this.dataProvider.get(key);
-  }
-
-  async delete(key: string): Promise<void> {
-    await this.dataProvider.delete(key);
-  }
-
-  async query(query: JsonSchema): Promise<T[]> {
-    return await this.dataProvider.query(query);
-  }
 }
