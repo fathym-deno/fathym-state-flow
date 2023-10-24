7ec54264ac55f3eb929f20c9126f11d964b72892
mcgear <michael.gearhardt@gmail.com>
2023-10-24 11:28:01 -0600

    scope

--- src/databases/DatabaseManager.ts
+++ src/databases/DatabaseManager.ts
@@ -1,7 +1,7 @@
 import { DatabaseSchema } from "./DatabaseSchema.ts";
 
 export class DatabaseManager {
-  constructor(private schema: DatabaseSchema) {}
+  constructor(protected schema: DatabaseSchema) {}
 
   async Generate() {
     // Implementation of the Generate method
