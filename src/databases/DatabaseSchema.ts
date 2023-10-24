84e15c9054c9c88c1438c015f3c98d6144916a36
mcgear <michael.gearhardt@gmail.com>
2023-10-24 11:26:49 -0600

    spacing

--- src/databases/DatabaseSchema.ts
+++ src/databases/DatabaseSchema.ts
@@ -1,11 +1,13 @@
 import { JsonSchema } from "./JsonSchema.ts";
 
 export class DatabaseSchema {
-  Name: string;
-  Database: Record<string, JsonSchema>;
+  public Name: string;
+
+  public Database: Record<string, JsonSchema>;
 
   constructor(name: string, database: Record<string, JsonSchema>) {
     this.Name = name;
+
     this.Database = database;
   }
 }
